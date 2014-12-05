91d1494b58eca823714780c013d1dd3747301063
mf222nb <mf222nb@student.lnu.se>
2014-12-05 16:49:09 +0100

    GJort det mesta av kraven

--- calls/TrafficMessageCall.php
+++ calls/TrafficMessageCall.php
@@ -2,9 +2,21 @@
 
 require_once("../rmccue/library/Requests.php");
 Requests::register_autoloader();
+$cachetime = 300;
+$cachefile = "traffic.json";
 
-//$request = Requests::get('http://api.sr.se/api/v2/traffic/messages?format=json&indent=true&size=100&sort=createddate+desc');
+
 
 //file_put_contents("traffic.json", $request->body);
-
-echo(file_get_contents("traffic.json"));
+if (file_exists($cachefile) && filemtime($cachefile) > (time() - 300 )) {
+    echo(file_get_contents("traffic.json"));
+}
+else{
+    $request = Requests::get('http://api.sr.se/api/v2/traffic/messages?format=json&size=100&sort=createddate');
+    if($request == "" || $request == null){
+        echo(file_get_contents("traffic.json"));
+        die();
+    }
+    file_put_contents($cachefile, $request->body);
+    echo(file_get_contents("traffic.json"));
+}
--- js/TrafficMessages.js
+++ js/TrafficMessages.js
@@ -5,12 +5,14 @@ var TrafficMessage = {
     array1:[],
     array2:[],
     array3:[],
+    array4:[],
     infoWindow:undefined,
     prev_InfoWindow:false,
-    chosenCategory:4,
+    chosenCategory:"4",
     init:function(){
         TrafficMessage.getMessages();
         TrafficMessage.getChosenCategoryClick();
+        TrafficMessage.getMessageListClick();
     },
     getMessages:function(){
         $.ajax({
@@ -37,73 +39,11 @@ var TrafficMessage = {
                     }
 
                     TrafficMessage.markers.push(data["messages"][i]);
-
-                    //date = new Date(parseInt(date.replace("/Date(", "").replace(")/",""), 10));
                 }
 
-                for (var i = 0; i < data["messages"].length; i++) {
-                    switch(TrafficMessage.chosenCategory) {
-                        case 0:
-                            var marker = new google.maps.Marker({
-                                map:GoogleMap.map,
-                                position: new google.maps.LatLng(TrafficMessage.array0[i].latitude, TrafficMessage.array0[i].longitude),
-                                title: TrafficMessage.array0[i].title
-                            });
-                            break;
-                        case 1:
-                            var marker = new google.maps.Marker({
-                                map:GoogleMap.map,
-                                position: new google.maps.LatLng(TrafficMessage.array1[i].latitude, TrafficMessage.array1[i].longitude),
-                                title: TrafficMessage.array1[i].title
-                            });
-                            break;
-                        case 2:
-                            var marker = new google.maps.Marker({
-                                map:GoogleMap.map,
-                                position: new google.maps.LatLng(array2[i].latitude, array2[i].longitude),
-                                title: TrafficMessage.array2[i].title
-                            });
-                            break;
-                        case 3:
-                            var marker = new google.maps.Marker({
-                                map:GoogleMap.map,
-                                position: new google.maps.LatLng(TrafficMessage.array3[i].latitude, TrafficMessage.array3[i].longitude),
-                                title: TrafficMessage.array3[i].title
-                            });
-                            break;
-                        case 4:
-                            var marker = new google.maps.Marker({
-                                map:GoogleMap.map,
-                                position: new google.maps.LatLng(TrafficMessage.markers[i].latitude, TrafficMessage.markers[i].longitude),
-                                title: TrafficMessage.markers[i].title
-                            });
-                            break;
-                    }
+                TrafficMessage.markers.reverse();
 
-                    var headerTitle = data["messages"][i].title;
-                    var id = data["messages"][i].id;
-                    TrafficMessage.renderTitle(headerTitle, id);
-                    /*var marker = new google.maps.Marker({
-                        position: new google.maps.LatLng(data["messages"][i].latitude, data["messages"][i].longitude),
-                        map: GoogleMap.map,
-                        title: data["messages"][i].title,
-                        category: data["messages"][i].category
-                    });*/
-                    //TrafficMessage.markers.push(marker);
-                    google.maps.event.addListener(marker, 'click', (function(marker) {
-                        var description = data["messages"][i].description;
-                        var date = data["messages"][i].createddate;
-                        var title = headerTitle;
-                        return function(){
-                            if(TrafficMessage.prev_InfoWindow){
-                                TrafficMessage.prev_InfoWindow.close();
-                            }
-                            TrafficMessage.renderInfoWindow(description, title, date);
-                            TrafficMessage.prev_InfoWindow = TrafficMessage.infoWindow;
-                            TrafficMessage.infoWindow.open(GoogleMap.map,marker);
-                        }
-                    })(marker));
-                }
+                TrafficMessage.getChosenCategoryMessages();
             }
         })
     },
@@ -111,12 +51,13 @@ var TrafficMessage = {
         var body = document.getElementById("messageList");
         var aTag = document.createElement("a");
 
-        aTag.innerText = title;
+        aTag.textContent = title;
         aTag.href = "#"+id;
+        aTag.id = id;
 
         body.appendChild(aTag);
     },
-    renderInfoWindow:function(description, title, date){
+    renderInfoWindow:function(description, title, date, subcategory){
         date = new Date(parseInt(date.replace("/Date(", "").replace(")/",""), 10));
         var contentString = '<div id="content">'+
             '<div id="siteNotice">'+
@@ -125,6 +66,7 @@ var TrafficMessage = {
             '<div id="bodyContent">'+
             '<p>' + date + '</p>'+
             '<p>' + description + '</p>'+
+            '<p>' + subcategory + '</p>'+
             '</div>'+
             '</div>';
 
@@ -132,15 +74,120 @@ var TrafficMessage = {
             content: contentString
         });
     },
-    getChosenCategoryClick:function(){
-        $('#selectList').change(function(v){
-            this.chosenCategory = v.target.value;
-            TrafficMessage.getMessages();
+    getMessageListClick:function(){
+        $("#messageList").on('click', function(v) {
+            for(var i = 0; i < TrafficMessage.array4.length; i++){
+                if(TrafficMessage.array4[i].id == v.target.id){
+                    if(TrafficMessage.prev_InfoWindow){
+                        TrafficMessage.prev_InfoWindow.close();
+                    }
+                    TrafficMessage.renderInfoWindow(TrafficMessage.array4[i].description, TrafficMessage.array4[i].title, TrafficMessage.array4[i].date, TrafficMessage.array4[i].subcategory);
+                    TrafficMessage.prev_InfoWindow = TrafficMessage.infoWindow;
+                    TrafficMessage.infoWindow.open(GoogleMap.map,TrafficMessage.array4[i]);
+                }
+            }
         });
     },
-    getChosenCategoryMessages:function(chosenCategory){
+    getChosenCategoryClick:function(){
+        $('#selectList').change(function(v){
+            TrafficMessage.chosenCategory = v.target.value;
+            TrafficMessage.getChosenCategoryMessages();
+        });
+    },
+    getChosenCategoryMessages:function(){
+        TrafficMessage.resetMarkers();
 
+        for (var i = 0; i < TrafficMessage.markers.length; i++) {
+            switch(TrafficMessage.chosenCategory) {
+                case "0":
+                    var marker = new google.maps.Marker({
+                        map:GoogleMap.map,
+                        position: new google.maps.LatLng(TrafficMessage.array0[i].latitude, TrafficMessage.array0[i].longitude),
+                        title: TrafficMessage.array0[i].title,
+                        date: TrafficMessage.array0[i].createddate,
+                        description: TrafficMessage.array0[i].description,
+                        id: TrafficMessage.array0[i].id,
+                        subcategory: TrafficMessage.array0[i].subcategory
+                    });
+                    TrafficMessage.array4.push(marker);
+                    TrafficMessage.renderTitle(TrafficMessage.array4[i].title, TrafficMessage.array4[i].id);
+                    break;
+                case "1":
+                    var marker = new google.maps.Marker({
+                        map:GoogleMap.map,
+                        position: new google.maps.LatLng(TrafficMessage.array1[i].latitude, TrafficMessage.array1[i].longitude),
+                        title: TrafficMessage.array1[i].title,
+                        date: TrafficMessage.array1[i].createddate,
+                        description: TrafficMessage.array1[i].description,
+                        id: TrafficMessage.array1[i].id,
+                        subcategory: TrafficMessage.array1[i].subcategory
+                    });
+                    TrafficMessage.array4.push(marker);
+                    TrafficMessage.renderTitle(TrafficMessage.array4[i].title, TrafficMessage.array4[i].id);
+                    break;
+                case "2":
+                    var marker = new google.maps.Marker({
+                        map:GoogleMap.map,
+                        position: new google.maps.LatLng(TrafficMessage.array2[i].latitude, TrafficMessage.array2[i].longitude),
+                        title: TrafficMessage.array2[i].title,
+                        date: TrafficMessage.array2[i].createddate,
+                        description: TrafficMessage.array2[i].description,
+                        id: TrafficMessage.array2[i].id,
+                        subcategory: TrafficMessage.array2[i].subcategory
+                    });
+                    TrafficMessage.array4.push(marker);
+                    TrafficMessage.renderTitle(TrafficMessage.array4[i].title, TrafficMessage.array4[i].id);
+                    break;
+                case "3":
+                    var marker = new google.maps.Marker({
+                        map:GoogleMap.map,
+                        position: new google.maps.LatLng(TrafficMessage.array3[i].latitude, TrafficMessage.array3[i].longitude),
+                        title: TrafficMessage.array3[i].title,
+                        date: TrafficMessage.array3[i].createddate,
+                        description: TrafficMessage.array3[i].description,
+                        id: TrafficMessage.array3[i].id,
+                        subcategory: TrafficMessage.array3[i].subcategory
+                    });
+                    TrafficMessage.array4.push(marker);
+                    TrafficMessage.renderTitle(TrafficMessage.array4[i].title, TrafficMessage.array4[i].id);
+                    break;
+                case "4":
+                    var marker = new google.maps.Marker({
+                        map:GoogleMap.map,
+                        position: new google.maps.LatLng(TrafficMessage.markers[i].latitude, TrafficMessage.markers[i].longitude),
+                        title: TrafficMessage.markers[i].title,
+                        date: TrafficMessage.markers[i].createddate,
+                        description: TrafficMessage.markers[i].description,
+                        id: TrafficMessage.markers[i].id,
+                        subcategory: TrafficMessage.markers[i].subcategory
+                    });
+                    TrafficMessage.array4.push(marker);
+                    TrafficMessage.renderTitle(TrafficMessage.array4[i].title, TrafficMessage.array4[i].id);
+                    break;
+            }
+
+            google.maps.event.addListener(marker, 'click', (function(marker) {
+                var description = TrafficMessage.array4[i].description;
+                var date = TrafficMessage.array4[i].date;
+                var title = TrafficMessage.array4[i].title;
+                var subcategory = TrafficMessage.array4[i].subcategory;
+                return function(){
+                    if(TrafficMessage.prev_InfoWindow){
+                        TrafficMessage.prev_InfoWindow.close();
+                    }
+                    TrafficMessage.renderInfoWindow(description, title, date, subcategory);
+                    TrafficMessage.prev_InfoWindow = TrafficMessage.infoWindow;
+                    TrafficMessage.infoWindow.open(GoogleMap.map,marker);
+                }
+            })(marker));
+        }
+    },
+    resetMarkers:function(){
+        for(var i = 0; i < TrafficMessage.array4.length; i++){
+            TrafficMessage.array4[i].setMap(null);
+        }
+        TrafficMessage.array4 = [];
+        document.getElementById("messageList").innerHTML = "";
     }
 }
-
 window.addEventListener('load', TrafficMessage.init);
